61f616fb519dce2b52b8ecefba4b11f840672d88
Louis Leffler <louisleffler@gmail.com>
2020-11-28 22:43:47 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,18 +7,15 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  assembled_aoh = [
-    {name:"Don Gately", occupation:"Live-in Staff"},
-    {name:"Joelle van Dyne", occupation: "Radio Personality" },
-    {name: "Kate Gompert", occupation: "None"},
-    {name: "Bruce Green", occupation: "Fan of Mildred"}
-    ]
-end
-end
+  assembled_aoh = [ DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G ]
+end 
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  literal_aoh = 
+    [{name: "Don Gately", occupation: "Live-in Staff"},
+    {name: "Joelle van Dyne", occupation:"Radio Personality"},
+    {name: "Kate Gompert", occupation: "None"},
+    {name: "Bruce Green", occupation: "Fan of Mildred"}]
 end
 
 def aoh_lookup(aoh, row, key)
